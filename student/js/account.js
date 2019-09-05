c21860e7f26a8c78345ea42c11ca6ee0c225c866
dyliang <dyliang1985@hotmail.com>
2019-09-05 17:28:26 +0800

    20190905

--- student/js/account.js
+++ student/js/account.js
@@ -9,54 +9,54 @@ $(function() {
     var $page = $(".pageAccount");
     $page.delegate(".update", "click", function(e) {
         var $this = $(this);
-        $this.closest("tr").find("input").show().siblings(".text").hide();
+        $this.closest(".row").find("input").show().siblings(".text").hide();
         $this.hide().siblings().show();
     });
     $page.delegate(".updatePw", "click", function(e) {
         var $this = $(this);
-        $this.closest("tr").find("input").show().siblings(".text").hide();
+        $this.closest(".row").find("input").show().siblings(".text").hide();
         $this.hide().siblings().show();
         $page.find(".rePw").show();
     });
     $page.delegate(".updateMobile", "click", function(e) {
         var $this = $(this);
-        $this.closest("tr").find("input,button").show().siblings(".text").hide();
+        $this.closest(".row").find("input,button").show().siblings(".text").hide();
         $this.hide().siblings().show();
     });
     
     $page.delegate(".cancle", "click", function(e) {
         var $this = $(this);
-        $this.closest("tr").find("input").hide().siblings().show();
+        $this.closest(".row").find("input").hide().siblings().show();
         $this.hide().siblings(".save").hide().siblings(".update").show();
     });
     $page.delegate(".cancleMobile", "click", function(e) {
         var $this = $(this);
-        $this.closest("tr").find("input,button").hide().siblings(".text").show();
+        $this.closest(".row").find("input,button").hide().siblings(".text").show();
         $this.hide().siblings(".save").hide().siblings(".updateMobile").show();
     });
     $page.delegate(".canclePw", "click", function(e) {
         var $this = $(this);
-        $this.closest("tr").find("input").hide().siblings().show();
+        $this.closest(".row").find("input").hide().siblings().show();
         $this.hide().siblings(".save").hide().siblings(".updatePw").show();
         $page.find(".rePw").hide();
     });
     $page.delegate("#saveMobile", "click", function(e) {
         var $this = $(this);
-        $this.closest("tr").find("input, button").hide().siblings(".text").show();
+        $this.closest(".row").find("input, button").hide().siblings(".text").show();
         $this.hide().siblings(".cancleMobile").hide().siblings(".updateMobile").show();
         toastr.success('保存成功');
         toastr.error('保存失败');
     });
     $page.delegate("#saveEmail", "click", function(e) {
         var $this = $(this);
-        $this.closest("tr").find("input").hide().siblings().show();
+        $this.closest(".row").find("input").hide().siblings().show();
         $this.hide().siblings(".cancle").hide().siblings(".update").show();
         toastr.success('保存成功');
         toastr.error('保存失败');
     });
     $page.delegate("#savePw", "click", function(e) {
         var $this = $(this);
-        $this.closest("tr").find("input").hide().siblings().show();
+        $this.closest(".row").find("input").hide().siblings().show();
         $this.hide().siblings(".canclePw").hide().siblings(".updatePw").show();
         $page.find(".rePw").hide();
         toastr.success('保存成功');
--- student/js/dossier_2.js
+++ student/js/dossier_2.js
@@ -6,12 +6,24 @@ $(function() {
         todayHighlight: true
     });
     $page.delegate("#submit1", "click", function(e) {
+        var $parent = $(this).hide().closest(".box");
+        $parent.find(".readonly,.btnUpdate").show();
+        $parent.find(".edit,.btns button,.btnUpload").hide();
         toastr.success('提交成功');
         toastr.error('提交失败')
     });
     $page.delegate("#submit2", "click", function(e) {
+        var $parent = $(this).hide().closest(".box");
+        $parent.find(".readonly,.btnUpdate").show();
+        $parent.find(".edit,.btns button,.btnUpload").hide();
         toastr.success('提交成功');
         toastr.error('提交失败')
-    });   
+    });
+    $page.delegate(".btnUpdate", "click", function(e) {
+        var $parent = $(this).hide().closest(".box");
+        $parent.find(".readonly").hide();
+        $parent.find(".edit,.btns button").show();
+        $parent.find(".btnUpload").css("display","inline-block");
+    }); 
     
 })
